c6ef08747a52e00da65e06f39044c8614a716d0a
SuJiaKuan <feabries@gmail.com>
2020-09-16 21:54:35 +0900

    Delete sentences that contain filtered words

--- article_extraction/const.py
+++ article_extraction/const.py
@@ -7,3 +7,57 @@ class TERMINAL_COLOR:
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
+
+
+FILTERED_WORDS = [
+    # Information about Social Media:
+    # Facebook / Instagram / Telegram / LINE/ YouTube.
+    "按讚",
+    "按個讚",
+    "追蹤",
+    "Instagram",
+    "instagram",
+    "IG",
+    "Facebook",
+    "facebook",
+    "FB",
+    "臉書",
+    "粉絲頁",
+    "粉絲專頁",
+    "粉絲團",
+    "粉專",
+    "Telegram",
+    "telegram",
+    "LINE",
+    "Line@",
+    "Line ID",
+    "YOUTUBE",
+    "YouTube",
+    # Information about external webpages.
+    "http",
+    "連結",
+    # Information about the store.
+    "店家資訊",
+    "餐廳資訊",
+    "地址",
+    "店名",
+    "官網",
+    "官方網站",
+    "電話",
+    "營業時間",
+    "時間：",
+    "時間:",
+    "國定假日",
+    "地圖",
+    # Information about more / related articles.
+    ["更多", "美食"],
+    "更多文章",
+    "延伸閱讀",
+    "延展閱讀",
+    "同步發表",
+    "懶人包",
+    "食記",
+    "美食",
+    # Information about fixed text about Pixnet.
+    "痞客邦",
+]
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from article_extraction.article import Article
+from article_extraction.util import contain_filtered_words
+from article_extraction.const import FILTERED_WORDS
 
 
 def main():
@@ -17,6 +19,11 @@ def main():
     articles = [Article(p) for p in html_paths]
 
     for article in articles:
+        for pargraph in article.paragraphs:
+            for sentence in pargraph.sentences:
+                if contain_filtered_words(sentence.text, FILTERED_WORDS):
+                    sentence.delete()
+
         print(article.title)
         print(article.author_name)
         print(article.get_text(color=True))
